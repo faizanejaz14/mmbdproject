b5397342aef35014af94f0272a241cb33f50da75
Irtaza Hyder <irtazahyder2002@gmail.com>
2023-12-15 17:08:19 +0500

    Updated README Integrated TCP Web Socket for Google Maps API

--- README.md
+++ README.md
@@ -1,13 +1,51 @@
-# Microprocessor & Microconroller based Design semester Project [Live Cloud Controlled Bot with Google Navigation aka tiny Google Cherrry-bot]
+# Robotic Car Controller Project
 
-Features
-1) Live Cloud based camera stream
-2) Data exchange via Cloud [coordinates and controls]
-3) Navigation using Google Maps JS API
-4) Onboard Live GPS reading to update position
-5) Display of Stream, manual controls and marking direction on Google maps ON local webpage
+## Contributors
 
-Contributions:
-1) Syed Irteza:  Navigation, API configuration
-2) Muhammad Zakria:  Cam Stream, Sockets, Cloud service
-3) Faizan Ijaz:  Web Integration
+- [Zakria Mehmood](https://github.com/ZakriaComputerEngineer):
+  - Implemented live camera streaming functionality
+  - Developed socket communication
+  - Configured and managed cloud services
+
+- [Faizan Ijaz](https://github.com/faizanejaz14):
+  - Integrated the web interface
+  - Collaborated on overall system integration
+
+- [Irtaza Hyder](https://github.com/SyedMIrtazaHyder):
+  - Worked on navigation functionality
+  - Configured Google Direction APIs for integration
+
+## Overview
+This project involves controlling a 4-wheel robotic car using a Raspberry Pi 3 as its controller. The car is equipped with a USB webcam for live streaming and a NEO 6M GPS module to record its current coordinates. These modules are connected to the Raspberry Pi. The car transmits this data to a cloud server for further processing.
+
+## Hardware
+The hardware setup includes:
+- Raspberry Pi 3 as the controller
+- USB webcam for live streaming
+- NEO 6M GPS module for recording coordinates
+
+## Website
+A locally hosted website has been developed for this project. It utilizes the Google Maps API to display and generate the path and direction the car will take based on the GPS coordinates. The website also hosts the live stream from the NGROCK server. Additionally, the override controls are integrated into this website in case GPS navigation fails.
+
+## Cloud Services
+The project utilizes the following cloud services:
+- **NGROCK:** Used for live streaming
+- **RemoteIT:** Utilized for sending GPS coordinates. RemoteIT employs a duplex TCP service to transmit manual override and navigation instructions to the robotic car.
+
+## Usage
+To use this project:
+1. Clone the repository.
+2. Set up the Raspberry Pi with the necessary modules and connections.
+3. Run the locally hosted website to control and monitor the car's movements.
+
+## Contribution
+Contributions to enhance and improve this project are welcome. Fork the repository, make your changes, and create a pull request with details about your modifications.
+
+## References
+- NEO 6M with Raspberry Pi: GPS: https://sparklers-the-makers.github.io/blog/robotics/use-neo-6m-module-with-raspberry-pi/
+- Raspberry Pi 3: https://www.raspberrypi.com/documentation/computers/raspberry-pi.html
+- Google Maps: 
+    - Generating JSON: https://developers.google.com/maps/documentation/directions/get-directions
+    - Embedded Google Maps: https://developers.google.com/maps/documentation/javascript/adding-a-google-map
+    - Map Markers: https://developers.google.com/maps/documentation/javascript/advanced-markers/overview
+    - Direction Service: https://developers.google.com/maps/documentation/javascript/examples/directions-simple & https://developers.google.com/maps/documentation/javascript/directions
--- WEB PAGE/map_marker_set.js
+++ WEB PAGE/map_marker_set.js
@@ -10,6 +10,7 @@ function welcome(){
 
 //For loading markers: const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
 let map, infoWindow;
+const REMOTEIT_URL = 'tcp://proxy61.rt3.io:36072';
 //let staticMapURL = `https://maps.googleapis.com/maps/api/staticmap?size=400x400&maptype=roadmap&markers=color:blue%7Clabel:S%7C11211%7C11206%7C11222&key=AIzaSyCCB7UocJCGGZO4BxsxQ24TCtTNJTujGN0&signature=Intzeger`
 
 async function initMap() {
@@ -62,11 +63,18 @@ async function initMap() {
   carImg.width = 30;
 
   //Initializing a  marker
-  const src_marker = new AdvancedMarkerElement({
+  let src_marker = new AdvancedMarkerElement({
     map,
     position: { lat: 33.621955642487734, lng: 72.95814350678089 },
-    content: carImg,//pinBackground.element,
+    content: carImg,
   });
+
+  let dest_marker = new AdvancedMarkerElement({
+    map,
+    position: { lat: 33.621955642487734, lng: 72.95814350678089 },//saves desintation value, but initially null
+    content: pinBackground.element,//pinBackground.element,
+  });
+
 
   const locationButton = document.createElement("button");
   src_cords.innerText = `Latitude: ${src_marker.position.lat.toPrecision(8)}
@@ -82,7 +90,7 @@ async function initMap() {
   //Clicking on Map
   // Configure the click listener.
   map.addListener("click", (mapsMouseEvent) => {  
-    const dest_marker = new AdvancedMarkerElement({
+    dest_marker = new AdvancedMarkerElement({
       map,
       position: { lat: mapsMouseEvent.latLng.lat(), lng: mapsMouseEvent.latLng.lng() },//saves desintation value, but initially null
       content: pinBackground.element,//pinBackground.element,
@@ -110,6 +118,9 @@ async function initMap() {
     
     var direction_table = document.getElementsByClassName("adp-directions")[0]
     console.log(direction_table)
+    //Sending the current destination to server
+    sendDestinationCoordinates({"latitude": dest_marker.position.lat.toPrecision(8),
+                                "longitude": dest_marker.position.lng.toPrecision(8)})
   });
 
   //Clicking on config button
@@ -138,8 +149,24 @@ async function initMap() {
     }
   });
 
-  //Setting path A to B
+  //tcp connection
+  socket = new WebSocket(REMOTEIT_URL.replace('tcp', 'ws'));
 
+  socket.onopen = () => {
+   console.log('Connected to server');
+  };
+  
+  socket.onmessage = (event) => {
+    const currentCoordinates = JSON.parse(event.data);
+    console.log('Received current coordinates:', currentCoordinates);
+    src_marker = updateCurrentCoordinates(currentCoordinates);
+    calculateAndDisplayRoute(directionsService, directionsRenderer, src_marker.position, dest_marker.position)
+  };
+  
+  socket.onclose = (event) => {
+    console.log('Connection closed:', event);
+  };
+  
 }
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer, srcLatLng, destLatLng) {
@@ -165,36 +192,21 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.open(map);
 }
 
-function downloadJSONFile(url, filename) {
-  fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok.');
-      }
-      return response.json();
-    })
-    .then(data => {
-      const jsonContent = JSON.stringify(data, null, 2);
-      const blob = new Blob([jsonContent], { type: 'application/json' });
+function updateCurrentCoordinates(coordinates) {
+  // Update your UI with the received coordinates
+  const srcCordsLabel = document.getElementById('src_cords');
+  srcCordsLabel.innerText = `Latitude: ${coordinates.latitude.toPrecision(8)}
+  Longitude: ${coordinates.longitude.toPrecision(8)}`;
+  const src_marker = new AdvancedMarkerElement({
+    map,
+    position: { lat: coordinates.latitude, lng: coordinates.longitude },
+    content: carImg,
+  });
+  return src_marker;
+}
 
-      // Create a link element
-      const link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
-      link.download = filename || 'data.json';
-
-      // Append the link to the body
-      document.body.appendChild(link);
-
-      // Programmatically click the link to trigger the download
-      link.click();
-
-      // Clean up: remove the link and revoke the object URL
-      document.body.removeChild(link);
-      window.URL.revokeObjectURL(link.href);
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
-    });
+function sendDestinationCoordinates(destination) {
+  socket.send(JSON.stringify(destination));
 }
 
 window.initMap = initMap;
